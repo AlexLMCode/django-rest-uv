8266b20c7dc703ac3111414ac0242f9dc9a4c799
AlexLMCode <alexlujanm@outlook.com>
2021-05-28 00:57:51 -0500

    added rest-framework

--- mysite/myapi/admin.py
+++ mysite/myapi/admin.py
@@ -1,3 +1,5 @@
+from .models import Hero
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Hero)
--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+
+class Hero(models.Model):
+    name = models.CharField(max_length=60)
+    alias = models.CharField(max_length=60)
+
+    def __str__(self) -> CharField:
+        return self.name
